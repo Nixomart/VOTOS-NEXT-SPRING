e6eb76f2504cf3dad33e6c30565400c76bae67df
Elias Solis <elias3847040@gmail.com>
2023-11-27 17:24:10 -0300

    carlos

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -5,7 +5,7 @@ export default function Loading() {
   return (
     <div className="flex h-screen flex-col w-screen items-center justify-center">
   <ImSpinner8 className="animate-spin text-6xl text-indigo-700" />
-  <p>ELIAS!!!kdaskdqle!</p>
+  <p>Carlos</p>
 </div>
   )
 }
