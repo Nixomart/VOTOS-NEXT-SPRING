d2b1ab0472ebd2ad2d63c777d593bdd77fb854b9
Nixomart <nixoma1@gmail.com>
2023-11-27 15:24:07 -0300

    averr

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,8 +3,9 @@ import { ImSpinner8 } from 'react-icons/im'
 
 export default function Loading() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="flex h-screen flex-col w-screen items-center justify-center">
   <ImSpinner8 className="animate-spin text-6xl text-indigo-700" />
+  <p>Cargando</p>
 </div>
   )
 }
